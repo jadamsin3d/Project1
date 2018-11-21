638ec0f4f423067ea085af9fc466e3cad12b07b7
jadamsin3d <jadamsin3d@gmail.com>
2018-11-20 21:41:33 -0800

    added search function to code so that the API would bring back results

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     let queryURL = "https://accounts.spotify.com/api/token";
     let token = null;
+    
 
     function GetToken() {
         $.ajax({
@@ -17,17 +18,21 @@ $(document).ready(function () {
 
     setInterval(() => GetToken(), 300000);
 
-    $(".searchBtn").on("click", function () {
-        // $.ajax({
-        //     url: queryURL,
-        //     crossDomain: true,
-        //     method: "POST",
-        //     data: { "grant_type": "client_credentials" },
-        //     contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-        //     headers: { "Authorization": "Basic Y2FkYWUyOTk4YTMwNDZlZWEzMGQ3ZGQ5OTZhZTg4ZmY6NmMxMmQyYmI3YzNmNGUwYmIxMDBmZmY1ZDZmMGY3OWI=" }
-        // }).then(function (response) {
-        //     console.log(response);
-        // });
+    $(".searchBtn").on("click", function (event) {
+        event.preventDefault();
+
+        let track = $(".searchField").val().trim();
+
+        let searchQRY = "https://api.spotify.com/v1/search?q=" + track + "&type=track%2Cartist&market=US&limit=25&offset=5";
+
+        $.ajax({
+            url: searchQRY,
+            method: "GET",
+            headers: { "Authorization": "Basic Y2FkYWUyOTk4YTMwNDZlZWEzMGQ3ZGQ5OTZhZTg4ZmY6NmMxMmQyYmI3YzNmNGUwYmIxMDBmZmY1ZDZmMGY3OWI=" },
+            json: true,
+        }).then(function (response) {
+            console.log(response);
+        });
     });
     GetToken();
 });
